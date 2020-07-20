40822db65dac49c527f33d26e3e48cc62b467989
Felix Schlueter <felix.schlueter@kit.edu>
2020-07-20 19:07:22 +0200

    Relax criteria to find all pulses with a polar angle of 0 deg.

--- interpolation_Fourier.py
+++ interpolation_Fourier.py
@@ -10,22 +10,22 @@ class interp2d_fourier:
         # Input: positions x and y as 1-D arrays
         radius = np.sqrt(x**2 + y**2)
         phi = np.arctan2(y, x) # uses interval -pi..pi
-        phi[np.abs(phi)<1.0e-9] = 0.0 # avoid pathology (needed?)
+        phi[np.abs(phi) < np.deg2rad(0.01)] = 0.0 # avoid pathology
         phi[phi<0] += 2*np.pi # put into 0..2pi for ordering
-        
+
         phi_sorting = np.argsort(phi)
         # Assume star-shaped pattern, i.e. radial # steps = number of (almost) identical phi-values
         # May not work very near (0, 0)
         test = phi[phi_sorting] - phi[phi_sorting][0]
         radial_steps = len(np.where(np.abs(test) < 0.0001)[0])
         phi_steps = len(phi_sorting) // radial_steps
-        
+
         phi_sorting = phi_sorting.reshape( (phi_steps, radial_steps) )
         indices = np.argsort(radius[phi_sorting], axis=1)
         for i in range(phi_steps): # Sort by radius; should be possible without for-loop...
             phi_sorting[i] = phi_sorting[i][indices[i]]
         ordering_indices = phi_sorting.T # get shape (radial_steps, phi_steps)
-    
+
         return ordering_indices
 
     @classmethod
@@ -36,7 +36,7 @@ class interp2d_fourier:
         cos_components[..., 0] *= 0.5
         cos_components[..., -1] *= 0.5
         sin_components = -2*np.imag(fourier)
-   
+
         return (cos_components, sin_components)
 
     def __init__(self, x, y, values, radial_method='cubic', fill_value='extrapolate'):
@@ -44,13 +44,13 @@ class interp2d_fourier:
         # Input: positions x, y as 1D-arrays, values as 1-D array
         # radial_method: the interp1d method (keyword 'kind'), default='cubic' for cubic splines
         # fill_value: the fill value to use for a radius outside the min..max radius interval from the input. Set to 'extrapolate' by default; accuracy outside the interval is limited
-        
+
         # Convert (x, y) to (r, phi), make 2d position array, sorting positions and values by r and phi
         radius = np.sqrt(x**2 + y**2)
 
         ordering_indices = self.get_ordering_indices(x, y)
         values_ordered = np.copy(values)[ordering_indices]
-        
+
         # FFT over the angular direction, for each radius
         self.angular_FFT = np.fft.rfft(values_ordered, axis=1)
         length = values_ordered.shape[-1]
@@ -58,23 +58,23 @@ class interp2d_fourier:
 
         # Store the (unique) radius values
         self.radial_axis = radius[ordering_indices][:, 0]
-        
+
         # Check if the radius does not vary along angular direction (with tolerance)
         if np.max(np.std(radius[ordering_indices], axis=1)) > 0.001 * np.min(radius):
             raise ValueError("Radius must be (approx.) constant along angular direction")
-        
+
         # Produce interpolator function, interpolating the FFT components as a function of radius
         self.interpolator_radius = intp.interp1d(self.radial_axis, self.angular_FFT, axis=0, kind=radial_method, fill_value='extrapolate') # Interpolates the Fourier components along the radial axis
-    
-        
+
+
     def __call__(self, x, y, max_fourier_mode=None):
         # Interpolate the input used in __init__ for input positions (x, y)
         # Input: positions x, y as float or N-D array
         # max_fourier_mode: optional cutoff, do Fourier sum up to (incl.) this mode
-        
+
         radius = np.sqrt(x**2 + y**2)
         phi = np.arctan2(y, x)
-    
+
         # Interpolate Fourier components over all values of radius
         fourier = self.interpolator_radius(radius)
         fourier_len = fourier.shape[-1]
@@ -83,7 +83,7 @@ class interp2d_fourier:
 
         limit = max_fourier_mode+1 if max_fourier_mode is not None else fourier_len
         mult = np.linspace(0, limit-1, limit).astype(int) # multipliers for Fourier modes, as k in cos(k*phi), sin(k*phi)
-    
+
         result = np.zeros_like(radius)
         if isinstance(phi, float):
             result = np.sum(cos_components[..., 0:limit] * np.cos(phi * mult)) + np.sum(sin_components[..., 0:limit] * np.sin(phi * mult))
@@ -94,16 +94,15 @@ class interp2d_fourier:
         return result
 
     # Some getters for the angular FFT, its radial interpolator function, and the radial axis points used
-    
-    def get_angular_FFT(self):
-        
-        return self.angular_FFT
-    
-    def get_angular_FFT_interpolator(self):
-        
-        return self.interpolator_radius
-    
-    def get_radial_axis(self):
-        
-        return self.radial_axis 
 
+    def get_angular_FFT(self):
+
+        return self.angular_FFT
+
+    def get_angular_FFT_interpolator(self):
+
+        return self.interpolator_radius
+
+    def get_radial_axis(self):
+
+        return self.radial_axis
